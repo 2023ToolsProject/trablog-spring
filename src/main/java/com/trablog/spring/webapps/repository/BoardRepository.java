9100f498ea46950382c2bf5430b8db06ca04aca9
1030pmy <1030pmy@gmail.com>
2023-11-03 22:54:45 +0900

    query 수정

--- src/main/java/com/trablog/spring/webapps/repository/BoardRepository.java
+++ src/main/java/com/trablog/spring/webapps/repository/BoardRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    @Query("select b from Board b where b.member = :member")
+    @Query("select b from Board b where b.member =:member")
     public List<Board> selectByUsername(Member member);
 
-    @Query("select b from Board b where b.member = :name and b.id = :id")
+    @Query("select b from Board b where b.member =:name and b.id =:id")
     public Board findByUsername(Member member, Long id);
 
-    @Query("delete from Board b where b.member = :member and b.id = :id")
+    @Query("delete from Board b where b.member =:member and b.id =:id")
     public void deleteByUsername(Member member, Long id);
 
     @Query("select b from Board b where b.title LIKE %:keyword% OR b.content LIKE %:keyword%")
